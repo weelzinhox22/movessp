1e665e833c582b6dc1c3250d10c7d9ae4f441efd
wel <weelzinhox22@gmail.com>
2025-03-27 20:02:46 -0300

    Atualização: renderizando página de login diretamente

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.tsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Login from './pages/Login'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Login />
+  </React.StrictMode>,
+)
